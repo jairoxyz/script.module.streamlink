1f88505e691e390e936e11e978a393630330cefc
jairoxyz <jairoxyz@noreply.com>
2022-09-20 20:33:38 +0200

    rtve plugin fix

--- resources/lib/streamlink/plugins/rtve.py
+++ resources/lib/streamlink/plugins/rtve.py
@@ -9,9 +9,9 @@ import logging
 import re
 from base64 import b64decode
 from io import BytesIO
-from urlparse import urlparse
+from urllib.parse import urlparse
 
-from streamlink.plugin import Plugin, PluginArgument, PluginArguments, pluginmatcher
+from streamlink.plugin import Plugin, PluginArgument, PluginError, PluginArguments, pluginmatcher
 from streamlink.utils import parse_json
 from streamlink.stream.ffmpegmux import MuxedStream
 from streamlink.stream.hls import HLSStream
@@ -22,7 +22,7 @@ log = logging.getLogger(__name__)
 
 
 class Base64Reader:
-    def __init__(self, data):
+    def __init__(self, data: str):
         stream = BytesIO(b64decode(data))
 
         def _iterate():
@@ -61,6 +61,14 @@ class Base64Reader:
             raise ValueError("Invalid chunk length")
         self.skip(4)
         return chunktype, chunkdata
+    
+    def __iter__(self):
+        self.skip(8)
+        while True:
+            try:
+                yield self.read_chunk()
+            except ValueError:
+                return
 
 
 class ZTNR:
@@ -105,17 +113,16 @@ class ZTNR:
     @classmethod
     def translate(cls, data):
         reader = Base64Reader(data.replace("\n", ""))
-        reader.skip(8)
-        chunk_type, chunk_data = reader.read_chunk()
-        while chunk_type != "IEND":
+        for chunk_type, chunk_data in reader:
+            if chunk_type == "IEND":
+                break
             if chunk_type == "tEXt":
                 content = "".join(chr(item) for item in chunk_data if item > 0)
-                if "#" not in content or "%%" not in content:  # pragma: no cover
+                if "#" not in content or "%%" not in content:
                     continue
                 alphabet, content = content.split("#", 1)
                 quality, content = content.split("%%", 1)
                 yield quality, cls._get_source(alphabet, content)
-            chunk_type, chunk_data = reader.read_chunk()
 
 
 @pluginmatcher(re.compile(
@@ -126,6 +133,7 @@ class Rtve(Plugin):
         PluginArgument("mux-subtitles", is_global=True),
     )
 
+    URL_M3U8 = "https://ztnr.rtve.es/ztnr/{id}.m3u8"
     URL_VIDEOS = "https://ztnr.rtve.es/ztnr/movil/thumbnail/rtveplayw/videos/{id}.png?q=v2"
     URL_SUBTITLES = "https://www.rtve.es/api/videos/{id}/subtitulos.json"
 
@@ -140,21 +148,26 @@ class Rtve(Plugin):
             pass
         if not self.id:
             return
-
+        
+        # check obfuscated stream URLs via self.URL_VIDEOS and ZTNR.translate() first
+        # self.URL_M3U8 appears to be valid for all streams, but doesn't provide any content in same cases
         try:
             urls = self.session.http.get(self.URL_VIDEOS.format(id=self.id)).text
             urls = list(ZTNR.translate(urls))
-        except:
-            return
-
-        url = next((url for _, url in urls if urlparse(url).path.endswith(".m3u8")), None)
-        if not url:
-            url = next((url for _, url in urls if urlparse(url).path.endswith(".mp4")), None)
-            if url:
-                yield "vod", HTTPStream(self.session, url)
-                pass
-            return
-
+            if not urls or len(urls) == 0:
+                raise PluginError   
+        except PluginError:
+            # catch HTTP errors and validation errors, and fall back to generic HLS URL template
+            url = self.URL_M3U8.format(id=self.id)
+                 
+        else:
+            url = next((url for _, url in urls if urlparse(url).path.endswith(".m3u8")), None)
+            if not url:
+                url = next((url for _, url in urls if urlparse(url).path.endswith(".mp4")), None)
+                if url:
+                    yield "vod", HTTPStream(self.session, url)
+                return
+        
         streams = HLSStream.parse_variant_playlist(self.session, url).items()
 
         if self.options.get("mux-subtitles"):
@@ -162,9 +175,9 @@ class Rtve(Plugin):
                 _src = self.session.http.get(self.URL_SUBTITLES.format(id=self.id)).text
                 subs = parse_json(_src)["page"]["items"]
             except:
-                pass
+                subs = []
 
-            if subs:
+            if subs and len(subs) > 0:
                 subtitles = {
                     s["lang"]: HTTPStream(self.session, update_scheme("https://", s["src"], force=True))
                     for s in subs
@@ -173,8 +186,7 @@ class Rtve(Plugin):
                     yield quality, MuxedStream(self.session, stream, subtitles=subtitles)
                 return
 
-        for stream in streams:
-            yield stream
+        yield from streams
 
 
 __plugin__ = Rtve
